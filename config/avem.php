e7abcf874669dfb0f2561eac8fa4c2126cbcb61e
Álex Puchades <alex94puchades@gmail.com>
2016-08-23 22:25:22 +0200

    Change default .env settings

--- config/avem.php
+++ config/avem.php
@@ -4,7 +4,7 @@ use Carbon\Carbon;
 
 return [
 
-	'period_start' => Carbon::createFromFormat('d/m', env('AVEM_PERIOD_START', '1/9')),
+	'period_start' => Carbon::createFromFormat('d/m', env('AVEM_PERIOD_START', '01/09')),
 
 	'mailchimp' => [
 		'member_list_id' => env('AVEM_MC_MEMBER_LIST_ID', ''),
