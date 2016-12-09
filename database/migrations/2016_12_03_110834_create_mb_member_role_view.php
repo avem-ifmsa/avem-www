a8832431573081c811f324c175223843e453120f
Álex Puchades <alex94puchades@gmail.com>
2016-12-09 15:29:57 +0100

    Minor edits

--- database/migrations/2016_12_03_110834_create_mb_member_role_view.php
+++ database/migrations/2016_12_03_110834_create_mb_member_role_view.php
@@ -15,10 +15,10 @@ class CreateMbMemberRoleView extends Migration
 	{
 		DB::statement('CREATE VIEW mb_member_role AS
 			SELECT DISTINCT mb_members.id AS mb_member_id, charge_role.role_id FROM mb_members
-				INNER JOIN mb_member_periods ON mb_members.id=mb_member_periods.mb_member_id
-				INNER JOIN charges ON mb_member_periods.charge_id=charges.id
-				INNER JOIN charge_role ON charges.id=charge_role.charge_id
-					WHERE CURRENT_TIMESTAMP BETWEEN mb_member_periods.start AND mb_member_periods.end
+			INNER JOIN mb_member_periods ON mb_member_periods.mb_member_id = mb_members.id
+			INNER JOIN charges ON charges.id = mb_member_periods.charge_id
+			INNER JOIN charge_role ON charge_role.charge_id = charges.id
+				WHERE CURRENT_TIMESTAMP BETWEEN mb_member_periods.start AND mb_member_periods.end
 		');
 	}
 
