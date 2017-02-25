780adaef30bd28aa4bcc7e8a1812df11e5b792f5
Álex Puchades <alex94puchades@gmail.com>
2017-02-25 21:41:12 +0100

    Configure to trust heroku reverse proxies

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
 		 */
  		Laravel\Tinker\TinkerServiceProvider::class,
  		Laravel\Socialite\SocialiteServiceProvider::class,
+		Fideloper\Proxy\TrustedProxyServiceProvider::class,
 
 		/*
 		 * Application Service Providers...
