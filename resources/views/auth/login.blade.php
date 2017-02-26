d394ee8b9f8dd8c65d0c701ade78a5cad505b461
Álex Puchades <alex94puchades@gmail.com>
2017-02-26 22:19:27 +0100

    Add logo to login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-	<form class="mt-3 col-md-8 offset-md-2" method="post" action="{{ route('login') }}">
+	<div class="mt-5 mb-3 text-center">
+		<img src="{{ asset('img/avem-logo.svg') }}">
+	</div>
+
+	<form class="mt-3 col-md-6 offset-md-3" method="post" action="{{ route('login') }}">
 		{{ csrf_field() }}
 
 		<p class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-			<label>Dirección de correo-e
-				<input class="form-control" name="email" type="email" required
-				       value="{{ old('email') }}" autofocus>
-			</label>
+			<label for="login-email">Dirección de correo-e</label>
+			<input id="login-email" class="form-control" name="email" required
+			       type="email" value="{{ old('email') }}" autofocus>
 			@if ($errors->has('email'))
 				<span class="form-text">
 					<strong>{{ $errors->first('email') }}</strong>
@@ -17,9 +20,9 @@
 		</p>
 
 		<p class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-			<label>Contraseña
-				<input class="form-control" name="password" type="password" required>
-			</label>
+			<label for="login-password">Contraseña</label>
+			<input id="login-password" class="form-control" name="password"
+			       type="password" required>
 			@if ($errors->has('password'))
 				<span class="form-text">
 					<strong>{{ $errors->first('password') }}</strong>
@@ -27,13 +30,13 @@
 			@endif
 		</p>
 
-		<p class="mt-4 mb-3 text-center">
+		<p class="mt-5 text-center">
 			<button class="btn btn-primary" type="submit">Iniciar sesión</button>
-		</p>
 
-		<p class="text-center">
-			<a class="mx-1" href="{{ route('register') }}">¿Todavía no eres socio?</a>
-			<a href="{{ route('password.request') }}">¿Has olvidado tu contraseña?</a>
+			<div class="mt-4 text-center">
+				<a class="mr-5" href="{{ route('register') }}">¿Todavía no eres socio?</a>
+				<a href="{{ route('password.request') }}">¿Has olvidado tu contraseña?</a>
+			</div>
 		</p>
 	</form>
 @stop
