82cd4b6cfcc27b5995208f1a36185efd554f4a20
Álex Puchades <alex94puchades@gmail.com>
2017-04-03 20:42:57 +0200

    Fix typo in MB member views

--- resources/views/admin/mbMembers/create.blade.php
+++ resources/views/admin/mbMembers/create.blade.php
@@ -5,7 +5,7 @@
 	<form method="post" action="{{ route('admin.mbMembers.store') }}">
 		{{ csrf_field() }}
 
-		@include('admin.mb_members.form', compact('users'))
+		@include('admin.mbMembers.form', compact('users'))
 
 		<p class="my-4 text-center">
 			<button class="btn btn-primary" type="submit">Crear miembro de junta</button>
--- resources/views/admin/mbMembers/edit.blade.php
+++ resources/views/admin/mbMembers/edit.blade.php
@@ -6,7 +6,7 @@
 		{{ csrf_field() }}
 		{{ method_field('put') }}
 
-		@include('admin.mb_members.form', compact('users', 'mbMember'))
+		@include('admin.mbMembers.form', compact('users', 'mbMember'))
 
 		<p class="my-4 text-center">
 			<button class="btn btn-primary" type="submit">Guardar miembro</button>
