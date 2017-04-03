1c28e7084df33a824185702f55fd58ba9078a697
Álex Puchades <alex94puchades@gmail.com>
2017-04-03 20:37:18 +0200

    Do not store user password as binary

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
 			$table->increments('id');
 			$table->string('name');
 			$table->string('surname');
-			$table->binary('password');
+			$table->string('password');
 			$table->string('email')->unique();
 			$table->string('photo')->nullable();
 			$table->string('gender')->nullable();
