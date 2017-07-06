5e9248f38686c8984fc0573599352a367d01b0b9
Álex Puchades <alex94puchades@gmail.com>
2017-07-06 20:24:38 +0200

    Add dependency on laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
  		Laravel\Socialite\SocialiteServiceProvider::class,
 		Fideloper\Proxy\TrustedProxyServiceProvider::class,
 		Spatie\MediaLibrary\MediaLibraryServiceProvider::class,
+		Barryvdh\Debugbar\ServiceProvider::class,
 
 		/*
 		 * Application Service Providers...
@@ -206,6 +207,7 @@ return [
 		'Cookie' => Illuminate\Support\Facades\Cookie::class,
 		'Crypt' => Illuminate\Support\Facades\Crypt::class,
 		'DB' => Illuminate\Support\Facades\DB::class,
+		'Debugbar' => Barryvdh\Debugbar\Facade::class,
 		'Eloquent' => Illuminate\Database\Eloquent\Model::class,
 		'Event' => Illuminate\Support\Facades\Event::class,
 		'File' => Illuminate\Support\Facades\File::class,
