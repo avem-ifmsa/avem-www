67d490aca4ff0c73a5cef742730a9136edbc00ed
Álex Puchades <alex94puchades@gmail.com>
2017-07-13 16:35:12 +0200

    Take advantage of morph maps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,16 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
+		// Make Carbon use app locale
 		Carbon::setLocale(config('app.locale'));
+
+		// Set up morph map for polymorphic relationships
+		Relation::morphMap([
+			'activity'           => 'Avem\Activity',
+			'plain_transaction'  => 'Avem\PlainTransaction',
+			'performed_activity' => 'Avem\PerformedActivity',
+			'working_group'      => 'Avem\WorkingGroup',
+		]);
 	}
 
 	/**
--- database/migrations/2017_02_26_012734_create_transactions_view.php
+++ database/migrations/2017_02_26_012734_create_transactions_view.php
@@ -15,10 +15,10 @@ class CreateTransactionsView extends Migration
 	{
 		DB::statement('CREATE VIEW transactions AS
 			SELECT concept, points, created_at, user_id, charge_period_id,
-				\'Avem\\PlainTransaction\' AS transactionable_type,
+				"plain_transaction" AS transactionable_type,
 				id AS transactionable_id FROM plain_transactions
 			UNION SELECT name AS concept, points, performed_activities.created_at,
-				user_id, charge_period_id, \'Avem\\PerformedActivity\' AS transactionable_type,
+				user_id, charge_period_id, "performed_activity" AS transactionable_type,
 				performed_activities.id AS transactionable_id FROM performed_activities
 					INNER JOIN activities ON activity_id = activities.id
 		');
