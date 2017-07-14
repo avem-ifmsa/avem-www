264d309de59e8abb201c54fb9c3a20ca35d30efb
Álex Puchades <alex94puchades@gmail.com>
2017-07-14 18:37:46 +0200

    Configure browserSync

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js').version();
 mix.sass('resources/assets/sass/app.scss', 'public/css').version();
+
+mix.browserSync('localhost:8000');
