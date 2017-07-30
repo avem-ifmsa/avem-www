6c7b5133692ab488ad4fe953980e0cb2ebdce423
Alex Puchades <alex94puchades@gmail.com>
2017-07-30 12:25:40 +0200

    Update dependencies (fixes npm@5.2.0/webpack issue)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,8 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js').version();
-mix.sass('resources/assets/sass/app.scss', 'public/css').version();
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .version();
 
 mix.browserSync({
 	proxy: {
