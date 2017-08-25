94bb0ec4c0c496f7b6498437f0a827824d62d94c
Alex Puchades <alex94puchades@gmail.com>
2017-08-25 12:07:28 +0200

    Temporarily disable carousel component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-
+	{{-- <div id="welcome-carousel" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner" role="listbox">
+			<div class="carousel-item active">
+				<img class="carousel-image" src="http://placehold.it/1200x400">
+			</div>
+			<div class="carousel-item">
+				<img class="carousel-image" src="http://placehold.it/1200x400">
+			</div>
+			<div class="carousel-item">
+				<img class="carousel-image" src="http://placehold.it/1200x400">
+			</div>
+		</div>
+	</div> --}}
 @stop
