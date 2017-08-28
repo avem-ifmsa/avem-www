7a7763b349932b3c78429478073e15c531df5251
Alex Puchades <alex94puchades@gmail.com>
2017-08-28 21:08:29 +0200

    Add user menu to admin page header

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,9 +7,45 @@
 				<a class="header-icon" href="{{ route('admin.index') }}">
 					{!! file_get_contents('img/avem-logo.svg') !!}
 				</a>
+
 				<h1 class="header-title">Panel de administración</h1>
+
+				<div class="l-user-menu">
+					<?php $user = Auth::user() ?>
+					<div class="user-menu user-menu--admin">
+						<button type="button" role="button" class="menu-toggle"
+						        data-toggle="collapse" data-target="#user-menu">
+							Hola, {{ $user->name }}
+							<i class="ml-1 fa fa-caret-down"></i>
+						</button>
+
+						<div id="user-menu" class="collapse l-menu-content menu-content">
+							<img class="user-image" src="{{ $user->profileImageUrl }}">
+
+							<div class="user-info">
+								<span class="user-name">{{ $user->fullName }}</span>
+								<span class="user-email">{{ $user->email }}</span>
+							</div>
+
+							@if ($user->hasActiveCharge)
+								<div class="user-status">
+									<span class="badge badge-info">
+										{{ $user->currentCharge->name }}
+									</span>
+								</div>
+							@endif
+
+							<form action="{{ route('logout') }}" method="post">
+								{{ csrf_field() }}
+								<button type="submit" class="logout-btn" role="button">
+									Cerrar sesión
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
 			</div>
-			
+
 			<div class="container">
 				@yield('content')
 			</div>
