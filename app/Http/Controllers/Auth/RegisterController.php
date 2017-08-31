40e5d895d55e16745cc6384cb6d0a5db6cc41ca6
Alex Puchades <alex94puchades@gmail.com>
2017-08-31 18:21:04 +0200

    Fix register controller redirect

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
 	 *
 	 * @var string
 	 */
-	protected $redirectTo = '/home';
+	protected $redirectTo = '/usuario';
 
 	/**
 	 * Create a new controller instance.
