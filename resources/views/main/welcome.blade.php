5f52304c98bd89ce4ebdcfc74981b478efe4aac7
Alex Puchades <alex94puchades@gmail.com>
2017-09-10 05:56:29 +0200

    Implement landing page

--- resources/views/main/welcome.blade.php
+++ resources/views/main/welcome.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.main')
 
 @section('content')
+	<div class="cta">
+		<h3 class="cta-title">
+			Porque la medicina no sólo está en los libros.
+		</h3>
+
+		<p class="cta-text">
+			Somos la asociación de estudiantes de medicina más grande de
+			la Universitat de València. No pases por la facultad como un mero
+			trámite. Conoce gente, aprende, sé crítico, y diviértete. Lo mejor
+			está por llegar.
+		</p>
+
+		<a href="https://blog.avem.es/qui%C3%A9nes-somos-80f42a6dd598"
+		   class="cta-button" role="button">Hazte socio</a>
+	</div>
+
 	{{-- <div id="welcome-carousel" class="carousel slide" data-ride="carousel">
 		<div class="carousel-inner" role="listbox">
 			<div class="carousel-item active">
