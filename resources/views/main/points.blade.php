65038f5a3ba08d8b670fdbd9dfb095bf1fa6eb48
KevinNaMo <KevinNaMo@users.noreply.github.com>
2018-09-19 14:53:38 +0200

    Add total_points to users interface

--- resources/views/main/points.blade.php
+++ resources/views/main/points.blade.php
@@ -3,6 +3,7 @@
 @section('home-content')
 	<section class="card p-4">
 		<h3>Transacciones de puntos</h3>
+		<p>Puntos totales<span>{{ $user->total_points }}</span></p>
 		<a href="https://docs.google.com/document/d/1WteKp_JXuFNrAYCxur1_E4FutD0xIHeOTM7BCLr0Xho/edit?usp=sharing"><p>Lista de socios ordenados según sus puntos</p></a>
 		<table class="table table-striped">
 			<thead>
